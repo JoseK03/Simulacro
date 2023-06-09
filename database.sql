93dd9aa6363135739410241f6664adcd91aeffca
JoseK <dajoseva@gmail.com>
2023-06-09 11:20:11 -0500

    avance

---
+++ database.sql
@@ -0,0 +1,30 @@
+CREATE DATABASE alquilartemis;
+
+USE alquilartemis;
+
+CREATE TABLE materiales(
+    id_material INT PRIMARY KEY AUTO_INCREMENT, 
+    nombre_material VARCHAR(50),
+    precio VARCHAR(50),
+)
+
+CREATE TABLE empleados(
+    id_empleado INT PRIMARY KEY AUTO_INCREMENT,
+    nombre_empleado VARCHAR(50),
+    contraseña VARCHAR(50),
+)
+
+CREATE TABLE clientes(
+    id_cliente INT PRIMARY KEY AUTO_INCREMENT,
+    nombre_cliente VARCHAR(50),
+    nit INT,
+)
+
+CREATE TABLE contizacion(
+    id_cotizacion INT PRIMARY KEY AUTO_INCREMENT,
+    id_empleado INT,
+    id_cliente INT,
+    fecha DATE,
+    hora VARCHAR(50),
+    total_a_pagar VARCHAR(50)
+)
